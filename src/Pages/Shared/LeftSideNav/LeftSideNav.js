5c21d8893b3c6ec9fe64ba4d5934936ab7c3d524
Md. Selim Hossain <mrrafi812345@gmail.com>
2023-09-02 22:05:31 +0600

    some-changes

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -6,7 +6,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect( () =>{
-        fetch('http://localhost:5000/news-categories')
+        fetch('https://news71-server.vercel.app/news-categories')
         .then( res => res.json())
         .then(data => setCategories(data));
     }, []);
